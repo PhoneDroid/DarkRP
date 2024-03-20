61ec4272a7455bbbe6f6e321dcc5a5e83def6c04
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2024-03-19 21:10:26 -0400

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -2,6 +2,59 @@
 echo "::error::
     Line 1
     Line 2
+
+    Test 1
+"
+
+echo "
+::error::
+    Line 1
+    Line 2
+
+    Test 2
+"
+
+echo "::error::
+Line 1
+    Line 2
+
+    Test 3
+"
+
+echo "::error::
+Line 1
+Line 2
+
+    Test 4
+"
+
+echo "
+::error::
+Line 1
+    Line 2
+
+    Test 5
+"
+
+echo "
+::error::
+Line 1
+Line 2
+
+    Test 6
+"
+
+echo "::error:: Line 1
+    Line 2
+
+    Test 7
+"
+
+echo "
+::error:: Line 1
+    Line 2
+
+    Test 8
 "
 
 echo "::error::Files from FSpectate have been edited. Please submit a PR to https://github.com/fptje/FSpectate instead!"

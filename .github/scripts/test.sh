f969b9e0df810df704db3d4f1b6264a78afb25a0
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2024-03-19 21:25:12 -0400

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,48 +1,5 @@
 
-echo "::error::
-    Line 1
-    Line 2
 
-    Test 1
-"
-
-echo "
-::error::
-    Line 1
-    Line 2
-
-    Test 2
-"
-
-echo "::error::
-Line 1
-    Line 2
-
-    Test 3
-"
-
-echo "::error::
-Line 1
-Line 2
-
-    Test 4
-"
-
-echo "
-::error::
-Line 1
-    Line 2
-
-    Test 5
-"
-
-echo "
-::error::
-Line 1
-Line 2
-
-    Test 6
-"
 
 echo "::error:: Line 1
     Line 2
@@ -58,5 +15,10 @@ echo "
 "
 
 echo "::error::Files from FSpectate have been edited. Please submit a PR to https://github.com/fptje/FSpectate instead!"
+
+echo "
+| :x: Files from **FSpectate** should be edited in it's repository! | [<kbd> <br> Repository <br> </kbd>](https://github.com/fptje/FSpectate)
+|:-|:-
+" >> $GITHUB_STEP_SUMMARY
 
 exit 1

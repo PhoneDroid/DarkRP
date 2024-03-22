5512c69143898f045e82c5bc48fd985e9a238afa
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2024-03-21 22:31:10 -0400

    Update Update-Dependencies.sh

--- .github/scripts/Update-Dependencies.sh
+++ .github/scripts/Update-Dependencies.sh
@@ -15,37 +15,7 @@ source "$( dirname "$0" )/Update-Dependency.sh"
 
 
 update  "dry=${dry}"                          \
-        'MySQLite'                            \
-        'https://github.com/FPtje/MySQLite'   \
-        'gamemode/libraries/mysqlite/'
-
-update  "dry=${dry}"                                        \
-        'PropProtection'                                    \
-        'https://github.com/fptje/falcos-Prop-protection'   \
-        'gamemode/modules/fpp/pp/'                          \
-        'lua/fpp'
-
-update  "dry=${dry}"                                \
-        'GModFunctional'                            \
-        'https://github.com/fptje/GModFunctional'   \
-        'gamemode/libraries/fn.lua'
-
-update  "dry=${dry}"                        \
-        'Simplerr'                          \
-        'https://github.com/fptje/simplerr' \
-        'gamemode/libraries/simplerr.lua'
-
-update  "dry=${dry}"                        \
-        'CAMI'                              \
-        'https://github.com/glua/CAMI'      \
-        'gamemode/libraries/sh_cami.lua'    \
-        'lua/autorun'
-
-update  "dry=${dry}"                            \
-        'Spectate'                              \
-        'https://github.com/fptje/FSpectate'    \
-        'gamemode/modules/fspectate/'           \
-        'lua/fspectate'
+        'MySQLite'
 
 
 function noCommits {
